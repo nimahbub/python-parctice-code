5e81cdd0b422258ba54408a31503a98c39d44e29
Nosin Ibna Mahbub <nosin.pic@gmail.com>
2018-06-28 23:29:18 +0600

    Add files via upload

---
+++ string/string.py
@@ -0,0 +1,18 @@
+first_name="Nosin Ibna"
+last_name="Mahbub"
+full_name=first_name+ " " +last_name
+print(full_name.upper())
+print(full_name.lower())
+print("hello," + full_name + "!")
+print("hello," ,last_name,"!")
+print("hello,",full_name,"!")
+print("hello,",full_name.upper(),"!")
+print("hello,",full_name.lower(),"!")
+print("hello,",full_name.title(),"!")
+print("My most used language:\n\tpython\n\tjavascipt\n\tc\n\tjava")
+fav="python"
+print(fav)
+fav_1=fav.rstrip
+print(fav_1)
+fav_2=fav.lstrip
+print(fav_2)
